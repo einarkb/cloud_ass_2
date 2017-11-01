cf4c83fe3739c8f67c042442c7b0fab95292cebb
Einar Budsted <budsted@hotmail.com>
2017-11-01 10:24:53 +0100

    fixed

---
+++ cmd/main.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"net/http"
+	"fmt"
+	"strings"
+	"../db"
+	"../fixer"
+)
+
+func handlerFunc(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func handlerLatest(w http.ResponseWriter, r *http.Request) {
+	langs := strings.Split(strings.Split(r.URL.Path, "latest/")[1], "/")
+	session := db.DbConnect()
+	if session == nil {
+		return
+	}
+	defer session.Close()
+
+	collection := session.DB("currencydb").C("tick")
+	dbSize, err := collection.Count()
+	if err != nil {
+		return
+	}
+	var data fixer.FixerPayload
+	collection.Find(nil).Skip(dbSize - 1).One(&data)
+
+	valid := isLanguageInputValid(data, langs)
+	if !valid {
+		fmt.Fprint(w, "Invalid currencies")
+		return
+	}
+
+	if langs[0] == "EUR" {
+		fmt.Fprint(w, data.Rates[langs[1]])
+	} else if langs[1] == "EUR" {
+		fmt.Fprint(w,  1 / data.Rates[langs[0]])
+	}else {
+		fmt.Fprint(w, data.Rates[langs[1]] / data.Rates[langs[0]])
+	}
+
+}
+
+func handlerAverage(w http.ResponseWriter, r *http.Request) {
+	langs := strings.Split(strings.Split(r.URL.Path, "average/")[1], "/")
+
+	session := db.DbConnect()
+	if session == nil {
+		return
+	}
+	defer session.Close()
+
+	collection := session.DB("currencydb").C("tick")
+	dbSize, err := collection.Count()
+	if err != nil {
+		return
+	}
+	var data[] fixer.FixerPayload
+	dif := 7 - dbSize
+	if dif < 0 {
+		dif = 0
+	}
+	collection.Find(nil).Skip(dbSize - 7 + dif).All(&data)
+	valid := isLanguageInputValid(data[0], langs)
+	if !valid {
+		fmt.Fprint(w, "Invalid currencies")
+		return
+	}
+
+	fmt.Fprint(w, len(data))
+	var lang0Avg, lang1Avg float64
+	for i := 0; i < len(data); i++ {
+		lang0Avg += data[i].Rates[langs[0]]
+		lang1Avg += data[i].Rates[langs[1]]
+	}
+	lang0Avg /= 7 - float64(dif)
+	lang1Avg /= 7 - float64(dif)
+
+	if langs[0] == "EUR" {
+		fmt.Fprint(w, lang1Avg)
+	} else if langs[1] == "EUR" {
+		fmt.Fprint(w,  1 / lang0Avg)
+	}else {
+		fmt.Fprint(w, lang1Avg / lang0Avg)
+	}
+}
+
+// checks if the specified currencies actually exists and are not duplicates.
+// if so, ok wil be set to true, otherwise false
+func isLanguageInputValid(data fixer.FixerPayload, langs[] string) bool {
+	var ok bool
+	for i := 0; i <= 1; i++ {
+		_, ok = data.Rates[langs[i]]
+		if !ok && langs[i] != data.Base {
+			break
+		} else {
+			ok = true
+		}
+	}
+	if langs[0] == langs[1] {
+		ok = false
+	}
+	if ok {
+		return true
+	} else {
+		return false
+	}
+}
+
+
+func main() {
+	fixer.Start()
+
+	http.HandleFunc("/", handlerFunc)
+	http.HandleFunc("/latest/", handlerLatest)
+	http.HandleFunc("/average/", handlerAverage)
+	http.ListenAndServe("127.0.0.1:8081", nil)
+}
